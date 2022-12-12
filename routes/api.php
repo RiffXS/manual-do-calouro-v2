5eafd90f2b84af29a95baf4d2927a4e39af89d66
Henrique Dalmagro <84425302+SimpleR1ick@users.noreply.github.com>
2022-12-11 21:01:49 -0300

    Router fix

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@
 include_once __DIR__.'/api/v1/default.php';
 
 // ROTAS API DO ANDROID
-include_once __DIR__.'api/v1/android.php';
+include_once __DIR__.'/api/v1/android.php';
 
 // ROTAS DE COMENTARIOS
-include_once __DIR__ .'/api/v1/comment.php';
+include_once __DIR__.'/api/v1/comment.php';
 
 // ROTAS DE CONTATOS
 include_once __DIR__.'/api/v1/contact.php';
